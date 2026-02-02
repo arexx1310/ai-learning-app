219196c8a7c12ac1416b1536c0145270b58cfcea
Archi Danva <archidanva@gmail.com>
2026-02-03 03:35:21 +0530

    Added frontend URL

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ connectDB();
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: process.env.FRONTEND_URL,
         methods: ["GET", "POST","PUT","DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true,
