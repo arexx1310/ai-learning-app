a7dd6103b637c1f88030e9b7fa41f6bdd2fb92b6
Archi Danva <archidanva@gmail.com>
2026-02-03 03:46:21 +0530

    Fixing api paths

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ connectDB();
 
 app.use(
     cors({
-        origin: process.env.FRONTEND_URL,
+        origin: `${process.env.FRONTEND_URL}`,
         methods: ["GET", "POST","PUT","DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true,
